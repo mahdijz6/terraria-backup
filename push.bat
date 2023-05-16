114b3196071e4195a99c5889f8b554f8cecd22a3
Hamid Reza <75851729+Hamid-Reza-mirai@users.noreply.github.com>
2023-05-16 12:56:26 +0330

    k

--- push.bat
+++ push.bat
@@ -2,6 +2,6 @@
 set /p commit_msg="Enter commit message: "
 git add .
 git commit -m "%commit_msg%"
-git push https://mahdijz6@github.com/mahdijz6/terraria-backup.git master
+git push https://mahdijz6@github.com/mahdijz6/terraria-backup.git main
 echo Done!
 pause
